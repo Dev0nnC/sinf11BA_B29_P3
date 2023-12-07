786b7d3c6da8f246996d0390d9ad1c7bc175838e
julien heyvaert <152927211+julienheyvaert@users.noreply.github.com>
2023-12-07 16:47:51 +0100

    Update etatAgitation.py

--- etatAgitation.py
+++ etatAgitation.py
@@ -1,22 +1,31 @@
-from microbit import *
-shakeStrength = 0
+def on_button_pressed_a():
+    basic.show_string("B")
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    basic.show_string("AB")
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    basic.show_string("")
+input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_gesture_shake():
-    global shakeStrength
-    shakeStrength = input.acceleration(Dimension.STRENGTH)
-    if shakeStrength < 1200:
-        radio.send_value("shakeStrengthState", 0)
-    elif shakeStrength >= 1200 and shakeStrength < 2000:
-        radio.send_value("shakeStrengthState", 1)
-    elif shakeStrength >= 2000:
-        radio.send_value("shakeStrengthState", 2)
+    if input.acceleration(Dimension.X) <= 500:
+        basic.show_icon(IconNames.SMALL_DIAMOND)
+        radio.send_value("shakeStrength", 0)
+    elif input.acceleration(Dimension.X) > 500 or input.acceleration(Dimension.X) <= 1000:
+        basic.show_icon(IconNames.SQUARE)
+        radio.send_value("shakeStrength", 1)
+    elif input.acceleration(Dimension.X) > 1000:
+        basic.show_icon(IconNames.SMALL_HEART)
+        radio.send_value("shakeStrength", 2)
     else:
-        basic.show_icon(IconNames.NO)
+        radio.send_value("shakeStrength", 3)
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
+radio.set_group(1)
+
 def on_forever():
-    global shakeStrength
-    basic.show_string("B")
-    shakeStrength = 0
-    radio.set_group(1)
+    basic.show_icon(IconNames.HOUSE)
 basic.forever(on_forever)
